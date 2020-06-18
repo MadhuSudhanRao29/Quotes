f8baebeb10afeb2d186b657fdb4f9859d8334fae
Madhu <madhan.urs3@gmail.com>
2020-06-18 21:02:49 +0530

    Quotes

---
+++ Quotes/QuotesTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  QuotesTableViewController.swift
+//  Quotes
+//
+//  Created by Madhu on 17/06/20.
+//  Copyright © 2020 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class QuotesTableViewController: UITableViewController
+{
+
+    var quotesToShow = [
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+    
+
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        
+        if ( quotesToShow.count  > 6 && quotesToShow.count == 12)
+        {
+            return quotesToShow.count
+        }
+      else
+        {
+       
+        return quotesToShow.count + 1
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "quoteCell", for: indexPath)
+
+        let row = indexPath.row
+        
+        if(row < quotesToShow.count)
+        {
+            cell.textLabel?.text          = quotesToShow[row]
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textColor     = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            cell.accessoryType            = .none
+        }
+        else
+        {
+           cell.textLabel?.text        = "Get More Quotes"
+            cell.textLabel?.textColor  = #colorLiteral(red: 0.262745098, green: 0.8470588235, blue: 0.7882352941, alpha: 1)
+            cell.accessoryType         = .disclosureIndicator
+           
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let row = indexPath.row
+        if(row == quotesToShow.count)
+        {
+            quotesToShow.append(contentsOf: premiumQuotes)
+            
+            tableView.reloadData()
+        }
+    
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+   
+}
